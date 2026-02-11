caf01a29155a37bb90f71592f5084388838b8233
Kimsejin-56 <saj5877@naver.com>
2026-02-11 19:05:47 +0900

    JPQL과 QUeryDSL 비교

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,12 +1,13 @@
-package study.querydsl.entity;
+package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 import static org.assertj.core.api.Assertions.*;
 
